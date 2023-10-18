49cd68361c39785c3544e4ec3e992d0a65640782
fares002 <fares@example.com>
2023-10-17 18:17:21 -0700

    fares

--- printchar.c
+++ printchar.c
@@ -1,15 +1,27 @@
-#include"main.h"
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * print_char - writes the character c to stdout
- * @arg: The character to print
+ * @c: The character to print
  *
  * Return: 1.
  */
-int print_char(va_list arg)
+int print_char(va_list c)
 {
 	unsigned char my_char;
 
-	my_char = va_arg(arg, int);
+	my_char = va_arg(c, int);
 	_putchar(my_char);
+	return (1);
+}
+/**
+ * print_porcentage - %
+ *
+ * Return: 1.
+ */
+int print_porcentage(void)
+{
+	_putchar('%');
 	return (1);
 }
