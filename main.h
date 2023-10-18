48024b0646777e2e5cd891c84601274e2714144e
fares002 <fares@example.com>
2023-10-17 17:21:50 -0700

    fares

--- main.h
+++ main.h
@@ -9,4 +9,18 @@
 int _printf(const char *format, ...);
 int printchar(va_list arg);
 int print_porcentage(void);
+
+
+/**
+  * struct code_format - Struct format
+  *
+  * @sc: The specifiers
+  * @f: The function associated
+  */
+typedef struct code_format
+{
+	char *sc;
+	int (*f)(va_list);
+} code_f;
+
 #endif
