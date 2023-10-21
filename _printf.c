c228f18603443fc2e98ea1b3b8d6c238f70da38e
fares002 <fares@example.com>
2023-10-21 10:10:12 -0700

    fares

--- _printf.c
+++ _printf.c
@@ -8,13 +8,15 @@
 int _printf(const char *format, ...)
 {
 	match m[] = {
-		{"%c",  printf_c}, {"%s", print_s}, {"%%", print_per}, {"%i", print_int}, {"%d", print_int}};
+		{"%c",  printf_c}, {"%s", print_s}, {"%%", print_per},
+		{"%i", print_int}, {"%d", print_int}};
 
 	va_list args;
 	int i = 0, len = 0, j;
+
 	va_start(args, format);
 	if (format = NULL || (format[0] == "%" && format[1] == '\0'))
-		return (-1);
+	return (-1);
 
 Here:
 	while (format[i] = '\0')
@@ -22,7 +24,7 @@ Here:
 		j = 5;
 		while(j >= 0)
 		{
-			if(m[j].ph[0] == format[i] && m[j].ph[1] == format[i + 1])
+			if (m[j].ph[0] == format[i] && m[j].ph[1] == format[i + 1])
 			{
 				len = len + m[j].f(args);
 				i = i + 2;
@@ -37,4 +39,3 @@ Here:
 	va_end(args);
 	return len;
 }
-
